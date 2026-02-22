347592de43592fe6efe6184199f9d194f839f304
Sagarchowdary3 <sagarchowdary99511@gmail.com>
2026-02-23 03:24:46 +0530

    PYTHON-Introduction

---
+++ Intro.py
@@ -0,0 +1,39 @@
+# print(123)
+# print(456)
+
+# the below line of code is Wrong
+# Print(789)
+
+''' Company Name : Stackly
+    Employee Name : Sagar Chowdary
+    Domine : DevOps Enginneer '''
+
+# a=10        or  Emp_ID=12345
+# print(a)        print(Emp_ID)
+
+# the below line of code is Wrong
+# print=10
+# print(print)
+
+# a=10
+# a=20
+# print(a)
+# Answer will be 20 because latest assigned value will be consider.
+
+# a=10 ---> Integer
+# b=10.5 ---> Float
+# c="Sagar" or Str1='Sagar'
+# str2='Chowdary'
+
+# print(type(a))
+# Answer will be <class 'int'> the inner most called function will be executed first.
+# print(type(b))
+# Answer will be <class 'float'> the inner most called function will be executed first.
+
+# print(a+b) ---> 20.5
+# print(a*2) ---> 20
+# print(a+c) ---> Error because we can't add int and str
+# print(str1+str2) ---> SagarChowdary
+# print(str1*2) ---> SagarSagar
+# print(str1*2+str2) ---> SagarSagarChowdary
+# 
